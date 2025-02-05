97b287246aa8b0a84eab6554eab2a9446747cc13
Yuri Mbalane <Yurimnbalane19@gmail.com>
2025-02-04 22:46:24 -0300

    First car methods

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
+    protected $fillable = ['nome', 'imagem'];
+
+    public function rules(){
+        return [
+            'nome' => 'required|unique:marcas,nome,'.$this->id.'|min:3',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedback(){
+
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca já existe',
+            'nome.min' =>'O nome deve ter no minimo 3 caracteres',
+        ];
+    }
 }
